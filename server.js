1448e232a4b6bbccb657924905f6186eee3469fe
lostluggagelx <lostluggagelx@gmail.com>
2025-06-15 14:10:59 +0100

    Serve index.html from backend

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 import express from 'express';
 import fs from 'fs';
+import path from 'path';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import fetch from 'node-fetch';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+const __dirname = path.resolve();
 
 const GOOGLE_SHEET_WEBHOOK_URL = 'https://script.google.com/macros/s/AKfycbzNk6XZkhZ8VpVwE_x-jO5DMDd_e4HnqtW9TyLEzj_Ayc1i2-5X_MGeChWctNhl8PBw/exec';
 
 app.use(cors());
 app.use(bodyParser.json());
+
+// Serve static files like index.html
+app.use(express.static(__dirname));
+
+// Serve index.html when visiting "/"
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.post('/submit', async (req, res) => {
   const { email } = req.body;
